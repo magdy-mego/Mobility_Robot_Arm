9dea490800108b4da5427497a50963fb0137d7a9
magdy-mego <magdyheshmat@outlook.com>
2025-06-01 19:19:55 +0200

    Add control logic comments for servo1 (up-down)

--- code/Arm_Robot/Arm_Robot.ino
+++ code/Arm_Robot/Arm_Robot.ino
@@ -18,11 +18,12 @@ char usechar = '0';
 
 void setup() {  
   Serial.begin(9600); // Start serial communication at 9600 bps
+
   mservo1.attach(servo1); // attaches the servo on pin 9 to the servo object
   mservo2.attach(servo2); // attaches the servo on pin 8 to the servo object
   mservo3.attach(servo3); // attaches the servo on pin 7 to the servo object
   mservo4.attach(servo4); // attaches the servo on pin 6 to the servo object
-
+  // Set default positions on startup
   mservo1.write(srv1);     
   mservo2.write(srv2); 
   mservo3.write(srv3); 
@@ -30,17 +31,18 @@ void setup() {
 }
 
 void loop() {
+  // Receive data from serial port
   if (Serial.available() > 0) {
-    Incoming_value = Serial.read();
-    usechar = Incoming_value;  
+    Incoming_value = Serial.read(); // Read the incoming byte
+    usechar = Incoming_value; // Store the incoming byte in a variable
   }
-
+  // Control servo 1 (base) up down
   if (usechar == 'A' && srv1 < 150) {
     srv1++;
     mservo1.write(srv1);
     delay(10);
   }
-
+  
   if (usechar == 'B' && srv1 > 50) {
     srv1--;
     mservo1.write(srv1);
